0c5b27b6b37cf7b4714180e8d9908b6ba2f37086
gentitope <gentitope@users.noreply.github.com>
2017-05-07 13:12:19 +0400

    Update bash3-bootstrap.sh

--- bash3-bootstrap.sh
+++ bash3-bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ## BOOTSTRAP ##
-#source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
-source $HOME/bash-oo-framework-master/lib/oo-bootstrap.sh
+source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
+#source $HOME/bash-oo-framework-master/lib/oo-bootstrap.sh
 
 ## MAIN ##
 #import util/log util/exception util/tryCatch util/namedParameters util/class
